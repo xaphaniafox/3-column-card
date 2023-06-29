0921d054675ffec0a8b8d879f75f9db8b5628ad4
xaphaniafox <f.hosseini64@gmail.com>
2023-06-29 19:36:26 +0300

    creating the first hard-coded card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import Card from "./components/Card";
 
 function App() {
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,14 @@ import Sedan from "../assets/svgs/icon-sedans.svg";
 
 const Card = () => {
   return (
-    <div className="position-absolute top-50 start-50 translate-middle">
-      <img src={Sedan} />
-      <h2>Sedans</h2>
-      <p>
+    <div className="card-container position-absolute top-50 start-50 translate-middle">
+      <img className="card--image" src={Sedan} />
+      <h2 className="card--header">Sedans</h2>
+      <p className="card--description">
         Choose a sedan for its affordability and excellent fuel economy. Ideal
         for cruising in the city or on your next road trip.
       </p>
+      <button className="card--button">Learn More</button>
     </div>
   );
 };
